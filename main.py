51da43f611fbcf4c6aa1adcfaf261c11bf9c1aee
Koan <70034019+ByKoan@users.noreply.github.com>
2024-12-19 20:01:22 +0100

    Added "Download playlist"

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ class YouTubeDownloaderApp:
 
         # Crear contenido de la pestaña de descarga de audio
         self.create_audio_download_tab()
+        
+        # Pestaña de Descarga de Playlist
+        self.playlist_download_tab = ttk.Frame(self.notebook)
+        self.notebook.add(self.playlist_download_tab, text="Download Playlist")
 
+        # Crear contenido de la pestaña de descarga de playlist
+        self.create_playlist_download_tab()
+        
         # Pestaña de Información del Creador
         self.creator_tab = ttk.Frame(self.notebook)
         self.notebook.add(self.creator_tab, text="Creator Info")
@@ -325,7 +332,115 @@ class YouTubeDownloaderApp:
                     self.audio_message_text.config(state=tk.DISABLED)
         else:
             messagebox.showwarning("Warning", "Please enter a valid YouTube URL, select a quality, select a format, and choose a download path.")
+    
+    def create_playlist_download_tab(self):
+        # Frame principal para la pestaña de descarga de playlists
+        main_frame = ttk.Frame(self.playlist_download_tab, padding="20")
+        main_frame.pack(fill=tk.BOTH, expand=True)
 
+        # Entrada de URL de la playlist
+        url_label = ttk.Label(main_frame, text="Enter YouTube playlist URL:")
+        url_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.playlist_url_entry = ttk.Entry(main_frame, width=70)
+        self.playlist_url_entry.grid(row=0, column=1, padx=10, pady=10, columnspan=2)
+
+        # Etiqueta para seleccionar tipo (video o audio)
+        type_label = ttk.Label(main_frame, text="Select download type:")
+        type_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.download_type_combobox = ttk.Combobox(main_frame, state="readonly", values=["Video", "Audio"])
+        self.download_type_combobox.grid(row=1, column=1, padx=10, pady=10)
+
+        # Etiqueta para seleccionar formato
+        format_label = ttk.Label(main_frame, text="Select format:")
+        format_label.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.playlist_format_combobox = ttk.Combobox(main_frame, state="readonly", values=["mp4", "mkv", "mp3", "m4a"])
+        self.playlist_format_combobox.grid(row=2, column=1, padx=10, pady=10)
+
+        # Etiqueta para seleccionar calidad
+        quality_label = ttk.Label(main_frame, text="Select quality:")
+        quality_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.playlist_quality_combobox = ttk.Combobox(main_frame, state="readonly", values=["High", "Medium", "Low"])
+        self.playlist_quality_combobox.grid(row=3, column=1, padx=10, pady=10)
+
+        # Botón para seleccionar la ruta de descarga
+        download_path_label = ttk.Label(main_frame, text="Select download path:")
+        download_path_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.playlist_download_path_entry = ttk.Entry(main_frame, width=70)
+        self.playlist_download_path_entry.grid(row=4, column=1, padx=10, pady=10)
+
+        browse_button = ttk.Button(main_frame, text="Browse", command=self.browse_playlist_download_path)
+        browse_button.grid(row=4, column=2, padx=10, pady=10)
+
+        # Botón para iniciar la descarga
+        download_button = ttk.Button(main_frame, text="Download Playlist", command=self.download_playlist)
+        download_button.grid(row=5, column=0, pady=20)
+
+        # Área de mensajes
+        self.playlist_message_text = tk.Text(main_frame, height=15, width=100)
+        self.playlist_message_text.grid(row=6, column=0, columnspan=3, padx=10, pady=10, sticky=tk.W+tk.E)
+
+        # Scrollbar para el área de mensajes
+        scrollbar = ttk.Scrollbar(main_frame, orient=tk.VERTICAL, command=self.playlist_message_text.yview)
+        scrollbar.grid(row=6, column=3, sticky=tk.NS)
+        self.playlist_message_text.config(yscrollcommand=scrollbar.set)
+
+    # Funcion para buscar seleccionar ruta de descarga de la playlist
+    def browse_playlist_download_path(self):
+        download_path = filedialog.askdirectory()
+        if download_path:
+            self.playlist_download_path_entry.delete(0, tk.END)
+            self.playlist_download_path_entry.insert(0, download_path)
+
+    # Funcion para descargar playlist enteras (Tiene que ser playlist publica)
+    def download_playlist(self):
+        url = self.playlist_url_entry.get().strip()
+        download_type = self.download_type_combobox.get()
+        format = self.playlist_format_combobox.get()
+        quality = self.playlist_quality_combobox.get()
+        download_path = self.playlist_download_path_entry.get().strip()
+
+        if url and download_type and format and quality and download_path:
+            self.playlist_message_text.config(state=tk.NORMAL)
+            self.playlist_message_text.delete(1.0, tk.END)
+            self.playlist_message_text.insert(tk.END, f"Downloading playlist from {url}\n")
+            self.playlist_message_text.config(state=tk.DISABLED)
+
+            ydl_opts = {
+                'format': f'bestaudio/best' if download_type == "Audio" else 'best',
+                'outtmpl': download_path + '/%(playlist_title)s/%(title)s.%(ext)s',
+                'merge_output_format': format if download_type == "Video" else None,
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': format,
+                    'preferredquality': '192',
+                }] if download_type == "Audio" else None,
+            }
+
+            if quality == "High":
+                ydl_opts['format'] = 'bestvideo+bestaudio/best'
+            elif quality == "Medium":
+                ydl_opts['format'] = 'best[height<=720]' # Si seleccionas la opcion "Medium" te descargara hasta una resolucion posible de 720p
+            elif quality == "Low":
+                ydl_opts['format'] = 'best[height<=480]' # Si seleccionas la opcion "Low" te descargara hasta una resolucion posible de 480p
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                try:
+                    ydl.download([url])
+                    self.playlist_message_text.config(state=tk.NORMAL)
+                    self.playlist_message_text.insert(tk.END, "Download completed\n")
+                    self.playlist_message_text.config(state=tk.DISABLED)
+                except yt_dlp.DownloadError as e:
+                    self.playlist_message_text.config(state=tk.NORMAL)
+                    self.playlist_message_text.insert(tk.END, f"Error: {str(e)}\n")
+                    self.playlist_message_text.config(state=tk.DISABLED)
+        else:
+            messagebox.showwarning("Warning", "Please fill all fields correctly.")
+    
     def create_creator_tab(self):
         main_frame = ttk.Frame(self.creator_tab, padding="20")
         main_frame.pack(fill=tk.BOTH, expand=True)
@@ -333,7 +448,7 @@ class YouTubeDownloaderApp:
         creator_name_label = ttk.Label(main_frame, text="Creator: Koan")
         creator_name_label.pack(pady=10)
 
-        creator_description_label = ttk.Label(main_frame, text="Junior Dev Learning Python & C.")
+        creator_description_label = ttk.Label(main_frame, text="Im a Junior developer learning by my self low level developing, if you follow me on github appreciate its so much :).")
         creator_description_label.pack(pady=10)
 
         github_button = ttk.Button(main_frame, text="GitHub Profile", command=lambda: webbrowser.open("https://github.com/bykoan"))
